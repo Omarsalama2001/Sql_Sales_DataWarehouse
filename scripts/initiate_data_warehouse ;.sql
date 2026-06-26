16865e6cea76dbe514a780a2814a66203b5ea512
Salama <110498127+Omarsalama2001@users.noreply.github.com>
2026-06-26 18:21:11 +0300

    initial dataWarehouse created with schemas

---
+++ scripts/initiate_data_warehouse ;.sql
@@ -0,0 +1,23 @@
+
+USE master ;
+
+-- Drop and recreate the 'DataWarehouse' database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE; -- this allows you to delete the db
+    DROP DATABASE DataWarehouse;
+END;
+
+GO
+-- create the dataWarehouse
+create database DataWarehouse ;
+Go
+use DataWarehouse;
+GO
+
+-- create schemas 
+create schema bronze;
+GO
+create schema silver;
+GO
+create schema gold;
